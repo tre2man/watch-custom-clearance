5afb063cfd8e1a09517050ab6503c374038fe4ea
tre2man <kimtree3940@gmail.com>
2023-06-10 22:46:36 +0900

    promise 원래대로 설정

--- src/utils/ThemeState.tsx
+++ src/utils/ThemeState.tsx
@@ -5,30 +5,7 @@ export type ThemeColor = 'light' | 'dark';
 
 export const STORAGE_KEY = 'THEME_STATE';
 
-const getThemeState = async () => {
-  try {
-    const value = await AsyncStorage.getItem(STORAGE_KEY);
-    if (value !== null) {
-      return value;
-    }
-  } catch (e) {
-    // error reading value
-  }
-};
-
 export const ThemeState = atom<ThemeColor>({
   key: STORAGE_KEY,
-  default: new Promise((resolve, reject) => {
-    getThemeState()
-      .then(value => {
-        if (value) {
-          resolve(value as ThemeColor);
-        } else {
-          resolve('light');
-        }
-      })
-      .catch(e => {
-        resolve('light');
-      });
-  }),
+  default: 'light',
 });
